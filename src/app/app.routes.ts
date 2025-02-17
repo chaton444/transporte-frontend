9df5be19e544848a3c064fbd0ae4af520aeae63a
chaton444 <chaton444@outlook.com>
2025-02-16 20:19:58 -0600

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,12 @@ import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { authGuard } from './auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
-// el authGuard sirve para tener acceso a los componentes solo cuanto tiene el token por asi decirlo
+
+// El authGuard sirve para tener acceso a los componentes solo cuando tiene el token, por así decirlo.
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  {path: 'dashboard',component:DashboardComponent,canActivate:[authGuard]},// aqui se mira si sigue guardado el login para poder entrar directo al dashboard
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },  // Dashboard accesible sin login
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },  // Redirige al Dashboard por defecto
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },  // Solo accesible con token
 ];
