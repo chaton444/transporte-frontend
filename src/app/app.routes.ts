49c4be9184dc74c4251f59ee536366a31ada559d
chaton444 <chaton444@outlook.com>
2025-02-16 20:31:05 -0600

    cambiode ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },  // Dashboard accesible sin login
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },  // Redirige al Dashboard por defecto
-  { path: 'home', component: HomeComponent, canActivate: [authGuard] },  // Solo accesible con token
+  { path: 'dashboard', component: DashboardComponent },  // Dashboard accesible sin login  
+  { path: 'home', component: HomeComponent },  
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Solo accesible con token
 ];
